afc7cb2542c70eec4c1d760ac7d5603a7e5a2c26
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-09-19 16:20:42 +0300

    feat: added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import {Route, Routes} from "react-router-dom";
+import EventsPage from "./pages/EventsPage/EventsPage";
+import ParticipantPage from "./pages/ParticipantPage/ParticipantPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import Layout from "./components/Layout/Layout";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/participants" element={<ParticipantPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import {store} from "./redux/store.js";
 import {Provider} from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
