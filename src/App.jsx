df083cb560f30b1ee1ef756cd3870ba337da4f5e
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-09-19 17:01:51 +0300

    feat: if page not found => events page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import EventsPage from "./pages/EventsPage/EventsPage";
 import ParticipantPage from "./pages/ParticipantPage/ParticipantPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
@@ -12,6 +12,7 @@ function App() {
         <Route path="/participants" element={<ParticipantPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Route>
+      <Route path="*" element={<Navigate to="/events" replace />} />
     </Routes>
   );
 }
