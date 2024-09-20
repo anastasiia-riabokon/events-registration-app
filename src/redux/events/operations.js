f07a924de49165467731103cb211fda504768784
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-09-20 15:42:40 +0300

    fix: added params for pagination

--- src/redux/events/operations.js
+++ src/redux/events/operations.js
@@ -1,9 +1,9 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {myApi} from "../../config/myApi";
 
-export const getAllEvents = createAsyncThunk("events/getAll", async (_, thunkAPI) => {
+export const getAllEvents = createAsyncThunk("events/getAll", async (params, thunkAPI) => {
   try {
-    const {data} = await myApi.get("/events");
+    const {data} = await myApi.get("/events", {params});
     return data;
   } catch (error) {
     throw thunkAPI.rejectWithValue(error.message);
