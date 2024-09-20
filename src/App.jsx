a188ba362c69931b3241aabe9ed2b1dcd7eae9f0
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-09-20 14:18:04 +0300

    feat: lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import {Navigate, Route, Routes} from "react-router-dom";
-import EventsPage from "./pages/EventsPage/EventsPage";
-import ParticipantPage from "./pages/ParticipantPage/ParticipantPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import {Suspense, lazy} from "react";
+
 import Layout from "./components/Layout/Layout";
+import Loader from "./components/Loader/Loader";
+
+const EventsPage = lazy(() => import("./pages/EventsPage/EventsPage"));
+const ParticipantPage = lazy(() => import("./pages/ParticipantPage/ParticipantPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/events" element={<Layout />}>
-        <Route index element={<EventsPage />} />
-        <Route path="/events/:id/participants" element={<ParticipantPage />} />
-        <Route path="/events/:id/register" element={<RegisterPage />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/events" replace />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/events" element={<Layout />}>
+          <Route index element={<EventsPage />} />
+          <Route path="/events/:id/participants" element={<ParticipantPage />} />
+          <Route path="/events/:id/register" element={<RegisterPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/events" replace />} />
+      </Routes>
+    </Suspense>
   );
 }
 
